d2b982dd31419094fc29be9cad449289278eac28
kovaciardey <andrei.kovaci@gmail.com>
2024-05-06 17:29:52 +0100

    added better color logic on the tile class which averages colours which have a lower entropy

--- Assets/Scripts/MapTile.cs
+++ Assets/Scripts/MapTile.cs
@@ -81,19 +81,66 @@ public class MapTile
 	{
 		if (!IsCollapsed)
 		{
-			return new Color(0, 0, 0);
+			// not collapsed and can have all possible options
+			// show black
+
+			if (!HasBeenPropagated())
+			{
+				return new Color(0, 0, 0);
+			}
+
+			// else
+			
+			// average the possible remaining colours
+			// in theory there should be fewer than the max number
+			float totalR = 0f;
+			float totalG = 0f;
+			float totalB = 0f;
+		
+			foreach (KeyValuePair<Color, bool> kvp in TileSuperpositions)
+			{
+				if (kvp.Value)
+				{
+					totalR += kvp.Key.r;
+					totalG += kvp.Key.g;
+					totalB += kvp.Key.b;
+				}
+			}
+		
+			float avgR = totalR / TileSuperpositions.Count;
+			float avgG = totalG / TileSuperpositions.Count;
+			float avgB = totalB / TileSuperpositions.Count;
+		
+			return new Color(avgR, avgG, avgB);
 		}
 		
-		foreach (KeyValuePair<Color, bool> kvp in TileSuperpositions)
+		// in theory there should be only one that is true at this point
+		foreach (KeyValuePair<Color,bool> pair in TileSuperpositions)
 		{
-			if (kvp.Value)
+			if (pair.Value)
 			{
-				return kvp.Key;
+				return pair.Key;
 			}
 		}
 		
 		// just for the compiler
 		return new Color(0, 0, 0);
+	}
+	
+	// it just checks if any of the values are false
+	// aka, it has been propagetd at least once,
+	// it possible allowed tiles has been reduced
+	private bool HasBeenPropagated()
+	{
+		foreach (KeyValuePair<Color,bool> pair in TileSuperpositions)
+		{
+			if (!pair.Value)
+			{
+				return true; // Return true if a false value is found
+			}
+		}
+
+		return false; // Return false if no false values are found
 	}
 	
 	// need to figure out this bit here.. 
